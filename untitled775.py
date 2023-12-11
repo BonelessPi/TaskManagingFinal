90029c598d887e72e6f0723fdbc0e8194e3d05a6
Mevan9261 <71800270+Mevan9261@users.noreply.github.com>
2023-12-10 21:42:32 -0500

    Add files via upload
    UPDATED TO FIX POTENTIAL ISSUES

--- untitled775.py
+++ untitled775.py
@@ -20,42 +20,45 @@ class MyApp:
         self.open_tasks_button = tk.Button(root, text="Show Open Tasks", command=self.show_open_tasks)
         self.open_tasks_button.pack()
     def create_task_page(self):
-        new_window = tk.Toplevel(self.root)
-        create_task_frame = tk.Frame(new_window)
+        taskWindow = tk.Toplevel(self.root)
+        create_task_frame = tk.Frame(taskWindow)
         create_task_frame.pack()
-        self.task_issue = tk.Label(root, text="Issue:")
+        self.task_issue = tk.Label(taskWindow, text="Issue:")
         self.task_issue.pack()
-        self.task_issue = tk.Entry(root)
+        self.task_issue = tk.Entry(taskWindow)
         self.task_issue.pack()
 
-        self.task_description = tk.Label(root, text="Description:")
+        self.task_description = tk.Label(taskWindow, text="Description:")
         self.task_description.pack()
-        self.task_description = tk.Entry(root)
+        self.task_description = tk.Entry(taskWindow)
         self.task_description.pack()
 
-        self.task_status = tk.Label(root, text="Status:")
+        self.task_status = tk.Label(taskWindow, text="Status:")
         self.task_status.pack()
-        self.task_status = tk.Entry(root)
+        self.task_status = tk.Entry(taskWindow)
         self.task_status.pack()
 
  
-        self.add_task_button = tk.Button(root, text="Add Task", command=self.add_task)
+        self.add_task_button = tk.Button(taskWindow, text="Add Task", command=self.add_task)
         self.add_task_button.pack()
         
         
    
     
-
+    tasks = []
     def show_open_tasks(self):
+       for label in self.tasks:
+            label.destroy()
        open_tasks = self.db_manager.get_tasks_by_status("TODO")
        for task in open_tasks:
-            info = "Task ID: {task._id}, Status: {task.status}, Name: {task.displayname}"
+            info =  f"Task ID: {task._id}, Status: {task.status}, Name: {task.displayname}"
             task_label = tk.Label(self.main_frame, text=info)
             task_label.pack()
-
+            self.tasks.append(task_label)
+       
     def lookup_task_page(self):
-        new_window = tk.Toplevel(self.root)
-        lookup_task_frame = tk.Frame(new_window)
+        lookupWin = tk.Toplevel(self.root)
+        lookup_task_frame = tk.Frame(lookupWin)
         lookup_task_frame.pack()
         
 
